579c3e2a40f470f1008563f2a6dd28842cec0331
Auriles El Haddad <auriles0404@gmail.com>
2016-10-04 16:46:54 +0200

    Ajout de photos pour les avis des utilisateurs
    correction de quelques bugs

---
+++ Macathon/includes/form_4.php
@@ -0,0 +1,22 @@
+<?php	
+	if(empty($_POST['name4']) || empty($_POST['email4']) || empty($_POST['message']))
+	{
+		return false;
+	}
+	
+	$name4 = $_POST['name4'];
+	$email4 = $_POST['email4'];
+	$message = $_POST['message'];
+	
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from Macathon.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name4: $name4 \nEmail4: $email4 \nMessage: $message \n";
+	$headers = "From: contact@yoursite.com\n";
+	$headers .= "Reply-To: $email4";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
